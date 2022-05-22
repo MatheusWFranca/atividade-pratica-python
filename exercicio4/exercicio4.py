9cd15598835fdb00d912d8f05736feaa40fc4515
MatheusWFranca <mateusanches2@gmail.com>
2022-05-22 20:57:18 -0300

    Objetivos

--- exercicio4/exercicio4.py
+++ exercicio4/exercicio4.py
@@ -1,51 +1,48 @@
-from random import randint
-
 print('Bem-vindo ao Controle de Estoque da Bicicletária do Matheus Willian França')
 
 # cadastro das peças
-
 peca = {}
 pecas = []
 
 
-def cadastraPeca():
-    while True:
+def cadastraPeca(): # função para cadastrar a peça
+    while True: # loop
         print('Você Selecionou a opção cadastrar')
         try:
-            for i in range(4):
-                codigo = len(pecas) + 1
+            for i in range(4): # loop para o cadastro de peças
+                codigo = len(pecas) + 1 # captura o length do numero de peças cadastradas e adiciona + 1
                 peca['Codigo'] = codigo
                 print('Seu codigo é {}'.format(codigo))
                 peca['Nome:'] = input('Por favor entre com o nome da peça')
                 peca['Fabricante'] = input('Por favor entre com o FABRICANTE da peça')
                 peca['Valor'] = input('Por favor entre com o VALOR(R$) da Peça')
-                pecas.append(peca.copy())
+                pecas.append(peca.copy()) #adiciona a lista cadastrada ao dicionário peças
                 return
-        except ValueError:
+        except ValueError: #captura o erro de digitação do usuário
             print('Caracter inválido')
             continue
 
 
-def consultarPeca():
+def consultarPeca(): # função para consultar a peças
     while True:
         try:
             print('Você selecionou a opção de consultar peças')
-            consultar = int(input('Entre com a opção desejada:\n'
+            consultar = int(input('Entre com a opção desejada:\n' #input para o menu desejado
                                 '1-Consultar Todas as Peças\n'
                                 '2-Consultar Peças por código\n'
                                 '3-Consultar peças por Fabricate\n'
                                 '4-Retornar\n'))
             if consultar == 1:
-                for peca in pecas:
+                for peca in pecas: # o loop percore todas as peças cadastradas
                     print('-------')
-                    for key, value in peca.items():
+                    for key, value in peca.items():  #printa para o usuário todas as peças
                         print('{} : {}'.format(key, value))
 
             elif consultar == 2:
                 codigoPeca = int(input('Digite o CÓDIGO da Peça:'))
-                for peca in pecas:
+                for peca in pecas: # o loop percore todas as peças cadastradas
                     print('-------')
-                    if(peca['Codigo'] == codigoPeca):
+                    if(peca['Codigo'] == codigoPeca): # o loop percore todas as pecas que estiveram com o código digitado pelo usuário
                         for key, value in peca.items():
                             print('{} : {}'.format(key, value))
 
@@ -53,27 +50,29 @@ def consultarPeca():
                 fabricante = input('Digite o FABRICANTE da Peça:')
                 for peca in pecas:
                     print('-------')
-                    if(peca['Fabricante'] == fabricante):
+                    if(peca['Fabricante'] == fabricante): # o loop percore todas as pecas que estiveram com o nome do fabricante cadastrado
                         for key, value in peca.items():
                             print('{} : {}'.format(key, value))
 
-            elif consultar == 4:
+            elif consultar == 4: # se o usuário digitar 4, ele retorna para o começo do menu
                 return
             else:
                 print('opção desconhecida, tente novamente.')
 
         except ValueError:
-            print('')
-
+            print('Opção inválida... tente novamente')
+            continue
 
 def removerPeca():
     print('Você selecionou a opção de remover a peça')
-    entrada = int(input('Digite o código da peça que deseja remover:'))
-    for peca in pecas:
-        if(peca['Codigo'] == entrada):
-            pecas.remove(peca)
+    entrada = int(input('Digite o código da peça que deseja remover:')) # captura a entrada do código da peça
+    for peca in pecas: # Percorre todas as peças cadastradas
+        if(peca['Codigo'] == entrada): # verifica código digitado pelo usuário
+            pecas.remove(peca) # remove a peça pelo numero do código
             print('Peça removida com sucesso!')
-while True:
+
+
+while True: #Entrada principal dos menus
     try:
         opcao = int(input('Digite a opção desejada: \n'
                           '1-Cadastrar peça\n'
@@ -81,6 +80,8 @@ while True:
                           '3-Remover peça\n'
                           '4-Sair''\n'))
 
+
+        # verificação abaixo das entradas do usuário
         if opcao == 1:
             cadastraPeca()
 
@@ -100,7 +101,7 @@ while True:
             continue
 
     except ValueError:
-        print('digite um caracter inválido, tente novamente!')
+        print('digite um caracter válido, tente novamente!') # captura o erro de digitação
         continue
 
 
